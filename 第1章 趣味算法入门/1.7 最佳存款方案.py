f46c0c444fac0088db2ad8bfda87e17919ba9f42
yu123234345 <55644694+CeresBebop@users.noreply.github.com>
2026-07-09 17:03:22 +0800

    Add files via upload

---
+++ 第1章 趣味算法入门/1.7 最佳存款方案.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python 3
+# -*- coding: utf-8 -*-
+# @author: CeresBebop
+# @desc: 最佳存款方案
+
+
+if __name__ == "__main__":
+   i = 0
+   money = 0.0
+   while i < 5:
+      money = (money + 1000) / (1 + 0.0063 * 12)
+      i += 1
+   print("应该存入的钱数为: %0.2f" %money)
+   
